91b77c52362d6bf1812f05fa079ddb66beda8744
Stephen <me@stephenmeansme.com>
2026-03-11 22:26:07 -0400

    test: exception hierarchy importable

---
+++ tests/test_exceptions.py
@@ -0,0 +1,17 @@
+from deckslots.exceptions import (
+    CardError,
+    CategoryError,
+    DecklistError,
+    FileError,
+    ParseError,
+    SlotError,
+)
+
+
+def test_exception_hierarchy():
+    assert issubclass(CardError, DecklistError)
+    assert issubclass(SlotError, DecklistError)
+    assert issubclass(CategoryError, DecklistError)
+    assert issubclass(FileError, DecklistError)
+    assert issubclass(ParseError, DecklistError)
+    assert issubclass(DecklistError, Exception)
